de6326b0b2ad06c2bdc046ca299009888c2e40e7
Arnal Shoorukov <arnal.shoorukov@gmail.com>
2017-08-16 13:17:11 +0600

    second

---
+++ app/src/main/java/com/mosquefinder/arnal/prayertimesapp/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.mosquefinder.arnal.prayertimesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
